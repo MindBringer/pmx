22b8017b9713505d32b0563d623ea86c97455fd7
MindBringer <jan.jansen@outlook.de>
2025-08-07 15:35:36 +0200

    rag

---
+++ rag-backend/retriever.py
@@ -0,0 +1,28 @@
+from llama_index import VectorStoreIndex, StorageContext, load_index_from_storage
+from llama_index.readers import SimpleDirectoryReader
+from llama_index.embeddings import OllamaEmbedding
+import os
+
+STORAGE_DIR = "./storage"
+DOCS_DIR = "./documents"
+
+def build_or_load_index():
+    embed_model = OllamaEmbedding(
+        model="llama3",
+        host="http://ollama-llama3:11434"
+    )
+
+    if os.path.exists(STORAGE_DIR) and os.listdir(STORAGE_DIR):
+        storage_context = StorageContext.from_defaults(persist_dir=STORAGE_DIR)
+        return load_index_from_storage(storage_context)
+    else:
+        documents = SimpleDirectoryReader(DOCS_DIR).load_data()
+        index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+        index.storage_context.persist(persist_dir=STORAGE_DIR)
+        return index
+
+def query_index(question: str, top_k: int = 3) -> str:
+    index = build_or_load_index()
+    query_engine = index.as_query_engine(similarity_top_k=top_k)
+    response = query_engine.query(question)
+    return str(response)
